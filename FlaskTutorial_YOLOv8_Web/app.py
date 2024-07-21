a11302d53e9aa74e96e1909c1c9b3720cd91acdf
dnyanugarule <114602296+dnyanugarule@users.noreply.github.com>
2024-07-21 16:40:44 +0530

    Update and rename flaskapp.py to app.py

--- FlaskTutorial_YOLOv8_Web/app.py
+++ FlaskTutorial_YOLOv8_Web/app.py
@@ -86,5 +86,3 @@ def webapp():
     #return Response(generate_frames(path_x = session.get('video_path', None),conf_=round(float(session.get('conf_', None))/100,2)),mimetype='multipart/x-mixed-replace; boundary=frame')
     return Response(generate_frames_web(path_x=0), mimetype='multipart/x-mixed-replace; boundary=frame')
 
-if __name__ == "__main__":
-    app.run(debug=True)
